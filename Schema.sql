6e1b31c0648540ac04688d182aaddebb247fd6c9
Kasfil <kasf@tuta.io>
2020-12-29 17:19:55 +0700

    add user_favorit table

--- Schema.sql
+++ Schema.sql
@@ -51,6 +51,14 @@ CREATE TABLE IF NOT EXISTS `phone_code` (
   PRIMARY KEY `_id`
 ) ENGINE=InnoDB;
 
+CREATE TABLE IF NOT EXISTS `user_favorit` (
+  `_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `user_id` INT UNSIGNED NOT NULL,
+  `product_id` INT UNSIGNED NOT NULL,
+  `added_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY `_id`
+) ENGINE=InnoDB;
+
 ALTER TABLE `products`
   ADD CONSTRAINT 'FK_products_brands'
     FOREIGN KEY (`brand_id`) REFERENCES `brands`(`_id`)
@@ -73,4 +81,16 @@ ALTER TABLE `customers`
   ADD CONSTRAINT `FK_customer_phone_code`
     FOREIGN KEY (`phone_code_id`) REFERENCES `phone_code`(`_id`)
     ON DELETE SET NULL
+    ON UPDATE CASCADE;
+
+ALTER TABLE `user_favorit`
+  ADD CONSTRAINT `FK_user_favorit`
+    FOREIGN KEY (`user_id`) REFERENCES `users`(`_id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE;
+
+ALTER TABLE `user_favorit`
+  ADD CONSTRAINT `FK_favorit_user`
+    FOREIGN KEY (`product_id`) REFERENCES `products`(`_id`)
+    ON DELETE CASCADE
     ON UPDATE CASCADE;
