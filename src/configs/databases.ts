243d395942357f0113802d1af06da32eb84f6a52
Kasfil <kasf@tuta.io>
2021-01-01 15:33:34 +0700

    rename path and update env helper

--- src/configs/databases.ts
+++ src/configs/databases.ts
@@ -1,11 +1,11 @@
-import { connectionInterface } from '@intfc/databases'
-import env from '@common/env'
+import { connectionInterface } from 'interfaces/databases'
+import env from 'helpers/env'
 
 export const main: connectionInterface = {
   client: 'mysql',
-  host: env('DB_DEFAULT_HOST', 'localhost').toString(),
-  user: env('DB_DEFAULT_USER', 'B3lanja_root_user').toString(),
-  password: env('DB_DEFAULT_PASS', '').toString(),
-  database: 'belanja_core',
+  host: env('DB_DEFAULT_HOST', 'localhost'),
+  user: env('DB_DEFAULT_USER', 'root'),
+  password: env('DB_DEFAULT_PASS', ''),
+  database: env('DB_DEFAULT_DATABASE', 'belanja'),
   charset: 'utf8mb4'
 }
